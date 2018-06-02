bd9464dfce2bc16c917997015027faa770e48e71
Dan Suh <kaehops@gmail.com>
2018-06-02 18:51:36 +0900

    Initial classification

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import DataSet (readData)
+import EM (classifyInit, randomVariance, randomCentroids, classProbability)
 import Numeric.LinearAlgebra (meanCov, fromRows, fromList)
 
 main :: IO ()
@@ -12,3 +13,17 @@ main = do
       (mean, cov) = meanCov (fromRows $ map fromList [[1, 2], [1, 2], [0.9, 1.9]])
     print mean
     print cov
+
+    classified <- classifyInit genData
+    print classified
+
+    let nc = 5
+        dim = 2
+        lb = -10.0
+        hb = 10.0
+    randCents <- randomCentroids nc dim lb hb
+    print randCents
+    randVars <- randomVariance nc dim
+    print randVars
+    let probs = classProbability genData (zip randCents randVars)
+    print probs
--- src/EM.hs
+++ src/EM.hs
@@ -7,7 +7,7 @@ import Data.List (maximumBy)
 import Data.Ord (comparing)
 
 import DataSet (GeneratedData, sample)
-import Numeric.LinearAlgebra (Vector, R, fromList, Matrix, inv, (#>), dot, det, size)
+import Numeric.LinearAlgebra (Vector, R, fromList, Matrix, inv, (#>), dot, det, size, (><))
 
 -- make EM algorithm
 -- E-step
@@ -28,14 +28,18 @@ randomCentroid dim lowBound highBound = do
     return $ take dim rcs
 
 -- generate random cnetroids
+-- MUST RUN SEPARATE RANDOM PROCESSES
 randomCentroids :: Int -> Int -> Double -> Double -> IO [Vector R]
 randomCentroids numCenter dim lb hb = mapM (fromList <$>) centroids
   where
     centroids = replicate numCenter $ randomCentroid dim lb hb
 
--- initialize variance to 1.0
-initVariance :: Int -> [Double]
-initVariance = flip replicate 1.0
+-- initialize all covariance to 1.0
+randomVariance :: Int -> Int -> IO [Matrix R]
+randomVariance numcls dim = do
+    stdGen <- getStdGen
+    let rcs = randomRs (0.5, 1.5) stdGen
+    return $ replicate numcls $ (dim><dim) rcs
 
 -- caculate the bivariate gaussian probability of a sample data
 sampleGaussProb :: Vector R -> Matrix R -> Vector R -> Double
@@ -51,16 +55,35 @@ sampleGaussProb mean cov samp = baseterm * expTerm
 argmax :: Ord a => [a] -> Int
 argmax = fst . maximumBy (comparing snd) . zip [0..]
 
+-- classify samples based on current parameters
 classify :: GeneratedData -> [(Vector R, Matrix R)] -> [[Vector R]]
 classify gendata params =
     foldl
       (\arr samp ->
-            let idx = argmax $ map ($ samp) probCalculator
-            in take idx arr ++ (samp:(arr !! idx)):drop (idx + 1) arr)
+            let idx = argmax $ map ($ samp) probCalculator  -- find the idx that gives most probability
+            in take idx arr ++ (samp:(arr !! idx)):drop (idx + 1) arr)  -- assign the sample
       (replicate numClasses [])
       samples
   where
-    samples = sample gendata
+    samples = sample gendata  -- generated data samples
     numClasses :: Int = length params
+    -- calculates the probability of a sample
     probCalculator :: [Vector R -> Double]
     probCalculator = [sampleGaussProb m c | (m, c) <- params]
+
+classProbability :: GeneratedData -> [(Vector R, Matrix R)] -> [[Double]]
+classProbability gendata params = [map (sampleGaussProb m c) samples | (m, c) <- params]
+  where
+    samples = sample gendata
+
+-- initial classification result
+classifyInit :: GeneratedData -> IO [[Vector R]]
+classifyInit gendata = do
+    randCents <- randomCentroids nc dim lb hb
+    randVars <- randomVariance nc dim
+    return $ classify gendata (zip randCents randVars)
+  where
+    nc = 5 -- number of classes
+    dim = 2  -- dimension of data
+    lb = -10.0
+    hb = 10.0
